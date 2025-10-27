847629b97e4f615b0bbb9d99fd57d4012272d874
Kaia <kaia.bryant@yahoo.com>
2025-10-27 18:47:04 -0400

    update env

--- client/src/api/tmbd.js
+++ client/src/api/tmbd.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:3000";
 
 export async function searchMovies(query) {
     const res = await fetch(`${BASE_URL}/search?title=${encodeURIComponent(query)}`);
